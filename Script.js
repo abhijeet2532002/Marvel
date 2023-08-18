8096d738770b2d0bf4f3897c084e2fe1e15b504e
abhij <abhijeetkumar2532002@gmail.com>
2023-08-18 09:08:19 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,221 @@
+// split URL into two parts
+let startUrl = 'http://gateway.marvel.com/v1/public/characters';
+let endUrl = '?&ts=1691554560791&apikey=0b0f563536c3a57a750d7aa1bb0dab54&hash=f3171302c1c54f0268f00e08fdd14c0f&limit=100';
+
+// Search bor (Input feild) varialble
+var searchBox = document.getElementById('searchBox');
+
+// get and set local storage data with key details
+localStorage.setItem('details', "");
+
+// get and set local storage data with key favourite
+if (localStorage.getItem('favourite') == null) {
+    localStorage.setItem('favourite', JSON.stringify([]));
+}
+
+// Fetch Home page data 
+function home() {
+    let fetchRes = fetch(startUrl + endUrl);
+    fetchRes.then(res => res.json()).then(d => {
+        var result = d.data.results;
+        var html = "";
+
+        result.forEach(element => {
+            let { path, extension } = element.thumbnail;
+
+            // check the card is listed in favourite or not
+            let fav_array = JSON.parse(localStorage.getItem('favourite'));
+            let favourite_icon = "";
+
+            if (fav_array.includes("" + element.id)) {
+                favourite_icon = `<i id="${element.id}"
+            class="favourite fa-solid fa-heart text-danger fs-5"></i>`;
+            } else {
+                favourite_icon = `<i id="${element.id}"
+            class="favourite fa-regular fa-heart text-danger fs-5"></i>`;
+            }
+
+            // Card design
+            html += `
+        <div class="col">
+            <!-- Card -->
+            <div class="card h-100 shadow-sm">
+                <!-- Card Image -->
+                <img src="${path + "." + extension}" class="card-img-top" style="height: 200px;" alt="...">
+
+                <!-- Card Body -->
+                <div class="card-body pt-2 pb-1">
+                    <!-- Card Body Heading -->
+                    <h6 class="card-title">${element.name}</h6>
+
+                    <div class="d-grid gap-1 col-12 mx-auto">
+                        <button id="/${element.id}/comics" class="comics btn btn-secondary fw-bolder fs-6 py-1" type="button">Comics : ${element.comics.available}</button>
+
+                        <button id="/${element.id}/series" class="series btn btn-secondary fw-bolder fs-6 py-1" type="button">Series : ${element.series.available}</button>
+                        
+                        <button id="/${element.id}/stories" class="stories btn btn-secondary fw-bolder fs-6 py-1" type="button">Stories : ${element.stories.available}</button>
+                    </div>
+                </div>
+
+                <!-- Card Footer -->
+                <div class="card-footer bg-transparent border border-0 my-0 pt-0">
+                    <!-- Details buttom -->
+                    <button id="${element.id}"
+                        class="viewDetails border border-top-0 border-start-0 border-end-0 border-dark border-2 bg-transparent p-0"
+                        style="float: left;"> View Details</button>
+                        
+
+                    <!-- Favourite buttom -->
+                    <button class="border border-0 bg-transparent p-1" style="float: right;">
+                        ${favourite_icon}
+                    </button>
+                </div>
+            </div>
+        </div>`;
+
+            document.getElementById("cardAppend").innerHTML = html;
+        });
+    });
+}
+home();
+
+// mouse clicked event handling
+function printMsg(e) {
+    let id = e.target.id;
+
+    // view Details
+    if (e.target.className.includes("viewDetails")) {
+        localStorage.setItem('details', id);
+        window.location.href = "./comics/Comics.html";
+    }
+
+    // mark or unmark as favorite
+    else if (e.target.className.includes("favourite")) {
+
+        // get data from local storage of key favourite
+        let array = JSON.parse(localStorage.getItem('favourite'));
+        let index = array.indexOf(id);
+
+        // Check id available or not
+        if (array.indexOf(id) == "-1") {
+            array.push(id);
+        } else {
+            array.splice(index, 1);
+        }
+
+        // Altering favourate data
+        localStorage.setItem('favourite', JSON.stringify(array));
+        home();
+    }
+
+    // favourite sidebar
+    else if (e.target.id == "favourite-button") {
+        // get data from local storage of key favourite
+        let favourite_id = JSON.parse(localStorage.getItem('favourite'));
+
+        // favourite card design
+        var favourite_div = "";
+        favourite_id.forEach(data => {
+
+            // fetch the character API of favourite list data 
+            let fetchResult = fetch(startUrl + "/" + data + endUrl);
+
+            // Display favourite card in favourite sidebar
+            fetchResult.then(res => res.json()).then(d => {
+                var result = d.data.results[0];
+                const { path, extension } = result.thumbnail;
+
+                favourite_div += `
+                <div class="col">
+                    <div class="card my-2">
+                        <img src="${path + "." + extension}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${result.name}</h5>
+                            <div class="row">
+                                <div class="d-flex justify-content-between">
+                                    <!-- Details buttom -->
+                                    <button id="${result.id}" class="viewDetails border border-top-0 border-start-0 border-end-0 border-dark border-2 bg-transparent p-0" style="float: left;"> View Details</button>
+                
+                                    <!-- Favourite buttom -->
+                                    <button class="border border-0 bg-transparent p-1" style="float: right;">
+                                        <i id="${result.id}" class="favourite fa-solid fa-heart text-danger fs-5"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+
+                document.getElementById("favourite-card-design").innerHTML = favourite_div;
+            });
+        });
+
+    }
+}
+
+// Input feild or search box handling
+function handleInput(e) {
+    let fetchSearch = fetch(startUrl + endUrl);
+    console.log(startUrl + endUrl);
+    html = "";
+    fetchSearch.then(res => res.json()).then(d => {
+        let data = d.data.results;
+
+        let filterData = data.filter(item => item.name.includes(searchBox.value));
+
+        filterData.forEach(element => {
+            let { path, extension } = element.thumbnail;
+            let fav_array = JSON.parse(localStorage.getItem('favourite'));
+            let favourite_icon = "";
+
+            if (fav_array.includes("" + element.id)) {
+                favourite_icon = `<i id="${element.id}"
+            class="favourite fa-solid fa-heart text-danger fs-5"></i>`;
+            } else {
+                favourite_icon = `<i id="${element.id}"
+            class="favourite fa-regular fa-heart text-danger fs-5"></i>`;
+            }
+            html += `
+                <div class="col">
+                <!-- Card -->
+                <div class="card h-100 shadow-sm">
+                    <!-- Card Image -->
+                    <img src="${path + "." + extension}" class="card-img-top" style="height: 200px;" alt="...">
+
+                    <!-- Card Body -->
+                    <div class="card-body pt-2 pb-1">
+                        <!-- Card Body Heading -->
+                        <h6 class="card-title">${element.name}</h6>
+
+                        <div class="d-grid gap-1 col-12 mx-auto">
+                            <button id="/${element.id}/comics" class="comics btn btn-secondary fw-bolder fs-6 py-1" type="button">Comics : ${element.comics.available}</button>
+
+                            <button id="/${element.id}/series" class="series btn btn-secondary fw-bolder fs-6 py-1" type="button">Series : ${element.series.available}</button>
+                            
+                            <button id="/${element.id}/stories" class="stories btn btn-secondary fw-bolder fs-6 py-1" type="button">Stories : ${element.stories.available}</button>
+                        </div>
+                    </div>
+
+                    <!-- Card Footer -->
+                    <div class="card-footer bg-transparent border border-0 my-0 pt-0">
+                        <!-- Details buttom -->
+                        <button id="${element.id}"
+                            class="viewDetails border border-top-0 border-start-0 border-end-0 border-dark border-2 bg-transparent p-0"
+                            style="float: left;"> View Details</button>
+                            
+
+                        <!-- Favourite buttom -->
+                        <button class="border border-0 bg-transparent p-1" style="float: right;">
+                            ${favourite_icon}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            `;
+            document.getElementById("cardAppend").innerHTML = html;
+        });
+    });
+}
+
+// adding click event listerner in whole document
+document.addEventListener('click', printMsg);
---
+++ comics/Comics.js
@@ -0,0 +1,337 @@
+// URL split into two parts
+let startUrl = 'http://gateway.marvel.com/v1/public/characters';
+let endUrl = '?&ts=1691554560791&apikey=0b0f563536c3a57a750d7aa1bb0dab54&hash=f3171302c1c54f0268f00e08fdd14c0f&limit=100';
+
+// read data from the local storage
+const data = localStorage.getItem('details');
+
+// card image document
+var pic = document.getElementById("pic");
+
+// Details related Variable
+var detailsTitle;
+var detailsDescription;
+var detailsComics;
+var detailsSeries;
+var detailsStories;
+var cardHeading;
+
+// card menu Function
+cardHeading = document.getElementById("cardHeading");
+
+// Card menu design
+cardHeading.innerHTML = `
+    <ul class="nav nav-tabs card-header-tabs" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a id="details" href="#" class="nav-link active" data-bs-toggle="tab" type="button" role="tab" aria-selected="true">Details</a>
+        </li>
+
+        <li class="nav-item" role="presentation">
+            <a id="${data}/stories" class="nav-link" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Stories</a>
+        </li>
+
+        <li class="nav-item" role="presentation">
+            <a id="${data}/series" class="nav-link" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Series</a>
+        </li>
+
+        <li class="nav-item" role="presentation">
+            <a id="${data}/comics" class="nav-link" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Comics</a>
+        </li>
+    </ul>
+`;
+
+// details card
+function detailCard() {
+    dataArea.innerHTML = `
+            <h4 class="card-title"><span>Character Name : </span><span>${detailsTitle}</span></h4>
+            <p class="card-text">
+                <div class="row">
+                    <div class="col-md-3">
+                        <h5>Description : </h5>
+                    </div>
+
+                    <div class="col-8">
+                        <p class="fs-6">${detailsDescription}</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <h6>Series No : </h6>
+                    </div>
+
+                    <div class="col-8">
+                        <p class="fs-6">${detailsSeries}</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <h6>Comic No : </h6>
+                    </div>
+
+                    <div class="col-8">
+                        <p class="fs-6">${detailsComics}</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <h6>Stories No : </h6>
+                    </div>
+
+                    <div class="col-8">
+                        <p class="fs-6">${detailsStories}</p>
+                    </div>
+                </div>
+            </p>
+        `;
+}
+
+// onload fuction 
+window.onload = () => {
+    let fetchRes = fetch(startUrl + "/" + data + endUrl);
+    fetchRes.then(res => res.json()).then(d => {
+        const { path, extension } = d.data.results[0].thumbnail;
+
+        detailsTitle = d.data.results[0].name;
+        detailsDescription = d.data.results[0].description;
+        detailsComics = d.data.results[0].comics.available;
+        detailsSeries = d.data.results[0].series.available;
+        detailsStories = d.data.results[0].stories.available;
+
+        pic.innerHTML = `
+            <img src="${path + "." + extension}"" class="img-fluid rounded-start h-md-100" style="width: 100%;" alt="image">
+        `;
+
+        detailCard();
+    });
+};
+
+// Menu Related variable
+let details = document.getElementById(`details`);
+let stories = document.getElementById(`${data}/stories`);
+let series = document.getElementById(`${data}/series`);
+let comics = document.getElementById(`${data}/comics`);
+
+// Details Function
+details.addEventListener('click', detailCard);
+
+
+// Stories Function
+function displayStories(e) {
+    e.preventDefault();
+
+    dataArea.innerHTML = `
+        <div class="row mx-1" >
+            <ul id="storyDetais" class="overflow-auto m-0 p-0" style="max-height: 500px; overflow-x: hidden !important;">
+            </ul>
+        </div >
+    `;
+
+    let fetchRes = fetch(startUrl + "/" + e.target.id + endUrl);
+
+    fetchRes.then(res => res.json()).then(d => {
+        var result = d.data.results;
+
+        let storyDetais = document.getElementById("storyDetais");
+        var html = "";
+
+        result.forEach(element => {
+            let items = element.creators.items;
+            var innerList = "";
+
+            items.forEach(data => {
+                innerList += `<li>${data.name} (role: ${data.role})</li>`;
+            });
+
+            html += `
+            <li class="border border-1 border-dark rounded-3 my-2">
+                <div class="row px-2">
+                    <div class="row">
+                        <span class="col-sm-3 col-md-2 fs-6 fw-bolder">Title : </span>
+                        <span class="col-sm-9 col-md-10 fs-6">${element.title}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-3 col-md-2 fs-6 fw-bolder">Creators : </span>
+                        <ul id="storyCreater" class="col-sm-8 9ol-md-10" type="none">
+                            ${innerList}
+                        </ul>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-3 col-md-2 fs-6 fw-bolder">Characters : </span>
+                        <span class="col-sm-9 col-md-10">${element.characters.available}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-3 col-md-2 fs-6 fw-bolder">Series : </span>
+                        <span class="col-sm-9 col-md-10">${element.series.available}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-3 col-md-2 fs-6 fw-bolder">Comics : </span>
+                        <span class="col-sm-9 col-md-10">${element.comics.available}</span>
+                    </div>
+                </div>
+            </li>`;
+
+            storyDetais.innerHTML = html;
+        });
+    });
+}
+stories.addEventListener('click', displayStories);
+
+
+// Series Function
+function displaySeries(e) {
+    e.preventDefault();
+    
+    dataArea.innerHTML = `
+        <div class="row mx-1" >
+            <ul id="seriesDetais" class="overflow-auto m-0 p-0" style="max-height: 500px; overflow-x: hidden !important;">
+            </ul>
+        </div >
+    `;
+
+    let fetchResult = fetch(startUrl + "/" + e.target.id + endUrl);
+    var html = "";
+    fetchResult.then(res => res.json()).then(d => {
+        
+        var result = d.data.results;
+
+        let seriesDetais = document.getElementById("seriesDetais");
+        var html = "";
+
+        result.forEach(element => {
+            let items = element.creators.items;
+            var innerList = "";
+
+            items.forEach(data => {
+                innerList += `<li>${data.name} (role: ${data.role})</li>`;
+            });
+
+            html += `
+            <li class="border border-1 border-dark rounded-3 my-2">
+                <div class="row px-2">
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Title : </span>
+                        <span class="col-sm-8 col-md-9 fs-6">${element.title}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Year : </span>
+                        <span class="col-sm-8 col-md-9 fs-6">${element.startYear} - ${element.endYear}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Rating : </span>
+                        <span class="col-sm-8 col-md-9 fs-6">${element.rating}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Creators : </span>
+                        <ul id="storyCreater" class="col-sm-8 9ol-md-10" type="none">
+                            ${innerList}
+                        </ul>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Characters : </span>
+                        <span class="col-sm-8 col-md-9">${element.characters.available}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Stories : </span>
+                        <span class="col-sm-8 col-md-9">${element.stories.available}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Comics : </span>
+                        <span class="col-sm-8 col-md-9">${element.comics.available}</span>
+                    </div>
+                </div>
+            </li>`;
+
+            seriesDetais.innerHTML = html;
+        });
+    });
+}
+series.addEventListener('click', displaySeries);
+
+
+// Comics Function
+function displayComics(e) {
+    e.preventDefault();
+    dataArea.innerHTML = `
+        <div class="row mx-1" >
+            <ul id="comicDetais" class="overflow-auto m-0 p-0" style="max-height: 500px; overflow-x: hidden !important;">
+            </ul>
+        </div >
+    `;
+
+    let fetchResult = fetch(startUrl + "/" + e.target.id + endUrl);
+    var html = "";
+    fetchResult.then(res => res.json()).then(d => {
+        
+        var result = d.data.results;
+
+        let comicDetais = document.getElementById("comicDetais");
+        var html = "";
+
+        result.forEach(element => {
+            let items = element.creators.items;
+            var innerList = "";
+
+            items.forEach(data => {
+                innerList += `<li>${data.name} (role: ${data.role})</li>`;
+            });
+
+            html += `
+            <li class="border border-1 border-dark rounded-3 my-2">
+                <div class="row px-2">
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Title : </span>
+                        <span class="col-sm-8 col-md-9 fs-6">${element.title}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Date : </span>
+                        <span class="col-sm-8 col-md-9 fs-6">${element.modified}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Digital Id : </span>
+                        <span class="col-sm-8 col-md-9 fs-6">${element.digitalId}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Creators : </span>
+                        <ul id="storyCreater" class="col-sm-8 9ol-md-10" type="none">
+                            ${innerList}
+                        </ul>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Characters : </span>
+                        <span class="col-sm-8 col-md-9">${element.characters.available}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Stories : </span>
+                        <span class="col-sm-8 col-md-9">${element.stories.available}</span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col-sm-4 col-md-3 fs-6 fw-bolder">Series : </span>
+                        <span class="col-sm-8 col-md-9">${element.series.name}</span>
+                    </div>
+                </div>
+            </li>`;
+
+            comicDetais.innerHTML = html;
+        });
+    });;
+}
+comics.addEventListener('click', displayComics);
